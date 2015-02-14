5e645c16c2a25088c1a5e5ac4091f6d9f81d3f5c
Paget Fink <pagetfink@gmail.com>
2015-02-14 17:36:08 -0500

    added string filters to lodash description and title returns

--- src/js/fetsy-listings.js
+++ src/js/fetsy-listings.js
@@ -6,6 +6,7 @@ app.showFetsyListings = function () {
   $.getJSON("https://openapi.etsy.com/v2/listings/active.js?includes=MainImage&geo_level=state&location=north carolina&api_key=08gz3o57xxzhamlkdgb8us19&callback=?")
   .done(function(data) {
     console.log(data);
+
     $('.container').html(storeTemplate({
       stores : data.results
     }));

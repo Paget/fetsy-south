2608bd9389e3f06e9b92b179aa4cddfb224f1770
Paget Fink <pagetfink@gmail.com>
2015-02-14 17:14:50 -0500

    added api description to item list

--- src/js/fetsy-listings.js
+++ src/js/fetsy-listings.js
@@ -3,24 +3,15 @@
   var storeTemplate = _.template(southernStores, {variable: 'm'});
 
 app.showFetsyListings = function () {
-  $.getJSON("https://openapi.etsy.com/v2/listings/active.js?includes=MainImage&geo_level=state&location=durham,north carolina&api_key=08gz3o57xxzhamlkdgb8us19&callback=?")
+  $.getJSON("https://openapi.etsy.com/v2/listings/active.js?includes=MainImage&geo_level=state&location=north carolina&api_key=08gz3o57xxzhamlkdgb8us19&callback=?")
   .done(function(data) {
-    console.log(data.results);
+    console.log(data);
     $('.container').html(storeTemplate({
       stores : data.results
     }));
 
   });
 
-
 };
 
-// make object
-// https://openapi.etsy.com/v2/listings/active.js?api_key=08gz3o57xxzhamlkdgb8us19&callback=foo
-
-// app.showFetsylistings = function () {
-//   $.getJSON("https://openapi.etsy.com/v2/listings/active.js?includes=MainImage&geo_level=state&location=arkansas&api_key=08gz3o57xxzhamlkdgb8us19&callback=?")
-//   .done(function(data) {
-//     console.log(data);
-//
 })();

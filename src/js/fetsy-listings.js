c0055a29909cfae2ee9ba1a8b553c012c4c18658
Paget Fink <pagetfink@gmail.com>
2015-02-15 18:00:47 -0500

    added header styling and grid layout with some mobile breakpoints

--- src/js/fetsy-listings.js
+++ src/js/fetsy-listings.js
@@ -3,7 +3,7 @@
   var storeTemplate = _.template(southernStores, {variable: 'm'});
 
 app.showFetsyListings = function () {
-  $.getJSON("https://openapi.etsy.com/v2/listings/active.js?includes=MainImage&geo_level=state&location=north carolina&api_key=08gz3o57xxzhamlkdgb8us19&callback=?")
+  $.getJSON("https://openapi.etsy.com/v2/listings/active.js?includes=MainImage&geo_level=state&location=north carolina&limit=8&api_key=08gz3o57xxzhamlkdgb8us19&callback=?")
   .done(function(data) {
     console.log(data);
 
@@ -15,4 +15,13 @@ app.showFetsyListings = function () {
 
 };
 
+  $('.container').on("click", '.store-info__pic', function(){
+    console.log('hello');
+    var listing_id = $(this).data("listing-id");
+    console.log('listing id', listing_id);
+    $.getJSON("https://openapi.etsy.com/v2/shops/listing/"+listing_id+".js?&api_key=08gz3o57xxzhamlkdgb8us19&callback=?")
+    .done(function(data) {
+      console.log(data);
+    })
+  });
 })();

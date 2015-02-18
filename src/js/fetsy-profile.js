add0d486ede4276f5fefc82563cef3b79fc3957e
Paget Fink <pagetfink@gmail.com>
2015-02-17 21:47:50 -0500

    added 404 page and got user profile view to display

--- src/js/fetsy-profile.js
+++ src/js/fetsy-profile.js
@@ -1,12 +1,17 @@
 app.showFetsyProfile = function(userId) {
 
-  // var southernStores = $('#southern-stores').html();
-  // var storeTemplate = _.template(southernStores, {variable: 'm'});
 
 var profileTemplate = _.template($('#user-profile').html(), { variable: 'm' });
 
+
 $.getJSON("https://openapi.etsy.com/v2/users/"+userId+"/profile.js?&api_key=08gz3o57xxzhamlkdgb8us19&callback=?")
 .done(function(data) {
-  console.log('user',data);
+        $('.container').html(profileTemplate({
+          user : data.results[0]
+        }));
+        $('.container2').html('');
+        $('.container3').html('');
+        console.log('user',data);
+
 });
 }
--- src/js/init.js
+++ src/js/init.js
@@ -2,6 +2,7 @@ function processHash() {
     var hash = location.hash || '#';
 
     if (!app.router.run(hash.slice(1))) {
+      console.error('404')
       app.pageNotFound();
     }
   }

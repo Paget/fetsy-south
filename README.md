575bebb857e7cc9987c56099aa5924ec23a7cdd1
Paget Fink <pagetfink@gmail.com>
2015-02-18 20:11:03 -0500

    Update 2015-02-19T01:11:00.884Z

--- README.md
+++
@@ -1,1 +0,0 @@
-//This app calls the etsy API to display latest listings for etsy shops located in the southern us. Current states: NC, AR, GA
--- src/js/page-not-found.js
+++
@@ -1,8 +0,0 @@
-app.pageNotFound = function () {
-      var pageNotFound = $('#page-not-found').html();
-      var pageNotFoundTemplate = _.template(pageNotFound, {variable: 'm'});
-
-      $('.container').html(pageNotFoundTemplate());
-      $('.container2').html('');
-      $('.container3').html('');
-  }
